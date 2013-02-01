a7e007663dd853dcda84e39ece117ddaa3c352e6
Tri Nguyen <tri@tridnguyen.com>
2013-01-31 21:45:06 -0500

    fix browser adding <br> for empty contenteditable div in kinda of a hackish way

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
 jQuery(document).ready(function ($) {
+    
     // Change the note title upon focus
     var default_title = $('.title').html();
-    $('.title').focus(function () {
+    $('.title').focus(function (e) {
         if ($(this).html() === default_title) {
             $(this).removeClass('default');
             $(this).html('');
         }
-    });
-    
-    $('.title').blur(function () {
-        if ($(this).html() === '') {
-            $(this).addClass('default');
-            $(this).html(default_title);
-        } else {
-            document.title = $(this).html();
+        // prevent browser default behavior, i.e. adding a <br> tag
+        // source: http://stackoverflow.com/questions/4038489/contenteditable-adds-a-br-when-i-hit-space
+        if (e.keyCode === 13 && e.shiftKey) { //enter && shift
+            e.preventDefault(); //Prevent default browser behavior
+            //this.html(this.html+"<br>");
+        }
+        if (e.keyCode === 13) { //enter
+            e.preventDefault(); //Prevent default browser behavior
         }
     });
+    
+    $('.title').blur(function (e) {
+        if ($(this).html() === '' || $(this).html() === '<br>') {
+            $(this).addClass('default');
+            $(this).html(default_title);
+        }
+        document.title = $(this).html();
+        
+        // prevent browser default behavior, i.e. adding a <br> tag
+        // source: http://stackoverflow.com/questions/4038489/contenteditable-adds-a-br-when-i-hit-space
+        if (e.keyCode === 13 && e.shiftKey) { //enter && shift
+            e.preventDefault(); //Prevent default browser behavior
+            //this.html(this.html+"<br>");
+        }
+        if (e.keyCode === 13) { //enter
+            e.preventDefault(); //Prevent default browser behavior
+        }
+    });
+    
+    
 });
